dc69cb5bd30f6a831800c227dc825263700a14ad
Arthur Santhiago Dias Luiz <142552481+Santh3ago@users.noreply.github.com>
2023-11-05 15:14:50 -0300

    Lista-Em-JavaScript
    Fiz a lista em script pelo html e direto no JS

---
+++ JavaScript/index.js
@@ -0,0 +1,31 @@
+//exercicio 1
+var resultado = 10 + 20;
+console.log(resultado);
+//exercicio 2
+var resultado = 30 - 10
+console.log(resultado)
+//exercicio 3
+var resultado = 2 * 15
+console.log(resultado)
+//exercicio 4
+var resultado = 100 / 5
+console.log(resultado)
+//exercicio 5
+var resto = 45 % 6;
+console.log(resto);
+//exercicio 6
+var numero1 = 15;
+var numero2 = 25;
+var numero3 = 35;
+var numero4 = 45;
+
+var soma = numero1 + numero2 + numero3 + numero4;
+var media = soma / 4;
+
+console.log(media);
+//exerciocio 7
+var valor = 80;
+var porcentagem = 25 / 100; // 0,25 é a representação decimal de 25%
+var resultado = valor * porcentagem;
+
+console.log(resultado);
