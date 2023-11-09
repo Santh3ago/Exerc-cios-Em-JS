c5de66440d82a2ffbb739018d649568064070249
Arthur Santhiago Dias Luiz <142552481+Santh3ago@users.noreply.github.com>
2023-11-09 19:26:21 -0300

    index.js

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,31 +1,31 @@
 //exercicio 1
-var resultado = 10 + 20;
+let resultado = 10 + 20;
 console.log(resultado);
 //exercicio 2
-var resultado = 30 - 10
+let resultado = 30 - 10
 console.log(resultado)
 //exercicio 3
-var resultado = 2 * 15
+let resultado = 2 * 15
 console.log(resultado)
 //exercicio 4
-var resultado = 100 / 5
+let resultado = 100 / 5
 console.log(resultado)
 //exercicio 5
-var resto = 45 % 6;
+let resto = 45 % 6;
 console.log(resto);
 //exercicio 6
-var numero1 = 15;
-var numero2 = 25;
-var numero3 = 35;
-var numero4 = 45;
+let numero1 = 15;
+let numero2 = 25;
+let numero3 = 35;
+let numero4 = 45;
 
-var soma = numero1 + numero2 + numero3 + numero4;
-var media = soma / 4;
+let soma = numero1 + numero2 + numero3 + numero4;
+let media = soma / 4;
 
 console.log(media);
 //exerciocio 7
-var valor = 80;
-var porcentagem = 25 / 100; // 0,25 é a representação decimal de 25%
-var resultado = valor * porcentagem;
+let valor = 80;
+let porcentagem = 25 / 100; // 0,25 é a representação decimal de 25%
+let resultado = valor * porcentagem;
 
 console.log(resultado);
